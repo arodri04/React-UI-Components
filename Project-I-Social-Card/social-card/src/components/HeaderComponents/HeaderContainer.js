eb58b944ed534cb82f5a916a197af4d24184ebae
arodri04 <augustine.rodriguez55@gmail.com>
2019-03-18 14:52:46 -0600

    Done No stretch

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+const Header = () => {
+  return (
+    <div className="headdiv">
+      <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+      <div>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Header from "./components/HeaderComponents/HeaderContainer.js";
+import CardContainer from "./components/CardComponents/CardContainer.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <Header />
+      <a href="ractjs.org">
+        <CardContainer />
+      </a>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
