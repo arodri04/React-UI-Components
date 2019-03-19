1a657aeb5cc7a0bf4c95b5d36c3dcdbb67f1ff1e
arodri04 <augustine.rodriguez55@gmail.com>
2019-03-19 15:04:55 -0600

    done mvp

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -14,5 +14,19 @@ function App() {
     </div>
   );
 }
-
+// const Header = () => {
+//   return (
+//     <div className="headt">
+//       <h1>
+//         Lambda School <span>@lambdaschool</span>
+//       </h1>
+//       <p className="headc">
+//         Let's learn React by building simple interfaces with components. Don't
+//         try to overthink it, just keep it simple and have fun. Once you feel
+//         comfortable using components, you are well on your way to mastering
+//         React!
+//       </p>
+//     </div>
+//   );
+// };
 ReactDOM.render(<App />, document.getElementById("root"));
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const NumberButton = props => {
+  if (Number.isInteger(props.numprop)) {
+    return <div className="numbut">{props.numprop}</div>;
+  } else {
+    return <div className="Actions">{props.numprop}</div>;
+  }
+};
+
+export default NumberButton;
